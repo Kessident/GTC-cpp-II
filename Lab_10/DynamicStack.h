ebbb9767212e7579d5e28407ee246c9967514194
windspinks <windspinks@gmail.com>
2019-06-17 10:32:01 -0400

    lab 10 start

---
+++ Lab_10/DynamicStack.h
@@ -0,0 +1,14 @@
+//
+// Created by Charles Dodge on 2019-06-17.
+//
+
+#ifndef LAB_10_DYNAMICSTACK_H
+#define LAB_10_DYNAMICSTACK_H
+
+
+class DynamicStack {
+
+};
+
+
+#endif //LAB_10_DYNAMICSTACK_H
---
+++ Lab_10/StaticStack.h
@@ -0,0 +1,14 @@
+//
+// Created by Charles Dodge on 2019-06-17.
+//
+
+#ifndef LAB_10_STATICSTACK_H
+#define LAB_10_STATICSTACK_H
+
+
+class StaticStack {
+
+};
+
+
+#endif //LAB_10_STATICSTACK_H

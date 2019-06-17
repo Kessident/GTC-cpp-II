80039f3e4040c572911039e369e0482d4d2021bf
windspinks <windspinks@gmail.com>
2019-06-17 10:33:41 -0400

    renamed vars

--- Classwork/main.cpp
+++ Classwork/main.cpp
@@ -7,38 +7,39 @@ using namespace std;
 int STACK_SIZE = 5;
 
 int main() {
-    IntStack stack (STACK_SIZE);
+    IntStack intStack (STACK_SIZE);
 
     cout << "---Pushing value onto the IntStack---\n";
     for (int i = 0; i < STACK_SIZE; ++i) {
         cout << i << endl;
-        stack.push(i);
+        intStack.push(i);
     }
 
     cout << "---Printing out values on IntStack--\n";
-    stack.print();
+    intStack.print();
 
     cout << "---Popping values off the IntStack--\n";
     for (int i = 0; i < STACK_SIZE; ++i) {
-        cout << stack.pop() << endl;
+        cout << intStack.pop() << endl;
     }
 
 
-    Stack<string> otherStack (STACK_SIZE);
-    string toBeAdded ("");
+    
+    Stack<string> stackTemp (STACK_SIZE);
+    string toBeAdded = ("");
     cout << "---Pushing value onto the Stack---\n";
     for (int i = 0; i < STACK_SIZE; ++i) {
         toBeAdded += "a";
         cout << toBeAdded << endl;
-        otherStack.push(toBeAdded);
+        stackTemp.push(toBeAdded);
     }
 
     cout << "---Printing out values on Stack--\n";
-    otherStack.print();
+    stackTemp.print();
 
     cout << "---Popping values off the Stack--\n";
     for (int i = 0; i < STACK_SIZE; ++i) {
-        cout << otherStack.pop() << endl;
+        cout << stackTemp.pop() << endl;
     }
     return 0;
 }
